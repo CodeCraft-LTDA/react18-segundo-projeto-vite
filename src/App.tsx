09da3af4265611f3b9c0cc199e6e1eb161ebf836
Sidnei <sidneisonic@hotmail.com>
2024-03-14 11:45:17 -0300

    Aula 30 Passando eventos como props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./components/Button";
+
 const App = () => {
   // function clicou() {
   //   alert('Clicou');
@@ -12,9 +14,11 @@ const App = () => {
       <h1>React 18</h1>
       <p>Segundo projeto com Vite</p>
       {/* <button onClick={clicou}>Clique aqui</button> */}
-      <button onClick={() => alert("clicou aqui")}>Clique aqui</button>
+      <button onClick={() => alert("clicou aqui")}>Aula 28</button>
 
-      <button onClick={() => handleClick("Teste")}>Clique aqui</button>
+      <button onClick={() => handleClick("Teste")}>Aula 29</button>
+
+      <Button label="Aula 30" onClick={() => handleClick("TEste")} />
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    label: string;
+    onClick: () => void;
+}
+
+export function Button({label, onClick}: Props) {
+    return (
+        <button onClick={onClick}>{label}</button>
+    )
+}
